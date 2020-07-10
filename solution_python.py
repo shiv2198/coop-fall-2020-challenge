dad95c54b0ffff6780de64e588a66a79eeb537ac
Shivansh Dave <s.k.dave21@gmail.com>
2020-07-10 14:37:51 -0400

    Update solution_python.py

--- solution_python.py
+++ solution_python.py
@@ -3,21 +3,66 @@ class EventSourcer():
 
     def __init__(self):
         self.value = 0
+        self.cur = -1
+        self.values = []
+        self.transactions = []
+        self.undos = 0
+        
 
     def add(self, num: int):
-        pass
+        self.value += num
+
+        if self.cur == -1 or len(self.values) == self.cur+1:
+            self.values.append(self.value)
+            self.transactions.append(num)
+        else:
+            self.values[self.cur + 1]  = self.value
+            self.transactions[self.cur + 1] = num
+            self.undos += 1
+
+        self.cur += 1
 
     def subtract(self, num: int):
-        pass
+        self.value -= num
+
+        if self.cur == -1 or len(self.values) == self.cur+1:
+            self.values.append(self.value)
+            self.transactions.append(-num)
+        else:
+            self.values[self.cur + 1]  = self.value
+            self.transactions[self.cur + 1] = -num
+            self.undos+=1
+
+        self.cur += 1
+
+
 
     def undo(self):
-        pass
+        self.cur -= 1
+        self.value = self.values[self.cur]
+        self.undos += 1
+
 
     def redo(self):
-        pass
+        self.cur += 1
+        self.value = self.value + self.transactions[self.cur]
+        self.undos -= 1
 
     def bulk_undo(self, steps: int):
-        pass
+        self.cur -= steps
+        if self.cur == -1:
+            self.value = 0
+        else:
+            self.value = self.values[self.cur]
+            self.undos += steps
 
     def bulk_redo(self, steps: int):
-        pass
+        step = min(steps, self.undos)
+        for i in range(step):
+            self.cur+=1
+            self.values[self.cur] = self.values[self.cur-1] + self.transactions[self.cur]
+            self.undos -= 1
+            print(self.values[self.cur])
+
+        # self.cur -= steps
+        value = self.values[self.cur]
